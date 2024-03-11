57b48efb6e54ee7650c8c0485c782f120d2fd5aa
Sivasish Praharaj <sivasish48@gmail.com>
2024-03-11 09:37:58 +0530

    readFile() function work along with its parameters and its mechanism.

--- 01/index.js
+++ 01/index.js
@@ -6,16 +6,20 @@
  //  When you call require() and pass a module name or path as an argument, Node.js searches for the specified module in the following locations:
 const fs = require("fs")
 
+
+// "theCallbk" is a javaScript function , this function will be called if an error occurs or if a data gets retrieved.
+// It has two parameter "err" which stands for error and another one is data
+// If there is an error "err" will contain information about it
+// Or if everything goes smoothly,"data" will contain the result.
 function theCallbk(err,data){
-    console.log(data);
+    console.log(data); // helooo
 }
+
+// here readFile() is a function in node.js to read the contents of a file(a.txt).
+// The "utf-8" parameter specifies that the file should be read as text using UTF-8 encoding. 
+//  The third parameter, theCallbk, is a callback function that will be called once the file is read.
+// So, when the file reading operation is complete, the theCallbk function will be executed with any error information (if there's an error) in the err parameter and the contents of the file in the data parameter.
 
 fs.readFile("a.txt","utf-8",theCallbk)
 
- // write a function
-
- function sum(num1,num2){
-    return num1+num2
- }
-
- console.log(sum(45,87));// 132
+  
