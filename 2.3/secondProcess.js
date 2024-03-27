ad4e1c5d40ee34f21c97ebae69e5e1c3b5864310
Sivasish Praharaj <sivasish48@gmail.com>
2024-03-28 01:38:51 +0530

    fetching from nodejs processess

---
+++ 2.3/secondProcess.js
@@ -0,0 +1,22 @@
+// As per we know that http request can only be sent by the browser and postman
+// But there is another way from where we can send http request to our bult in  http server
+// And that is called as the Nodejs processess
+
+// so let us create object which will contain our method
+
+const methodObj = {
+    method:"GET"
+}
+
+
+ function logData(jsonBody){
+    console.log(jsonBody);
+ }
+// now define the clbck here
+
+function theclbck(result){
+    result.json().then(logData)
+}
+// now use another external library of node js called fetch and put the route methodObj and keep a callback in the promise for its functionality
+
+fetch("http://localhost:3007/thecal",methodObj).then(theclbck)
