6564ce8d6b8c1c21b5b7314786670ea0c39d4ea9
Sivasish Praharaj <sivasish48@gmail.com>
2024-04-01 15:51:06 +0530

    understanding and handling core for flawless connection of backend with frontend

--- 3.0/Todo/todo.solution.file.js
+++ 3.0/Todo/todo.solution.file.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
 
+const path = require("path")
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -102,9 +104,37 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+
+//It is throwing cors error
+// CORS (Cross-Origin Resource Sharing) is a security feature implemented by web browsers to prevent malicious websites from making unauthorized requests to other websites. 
+// As we know fetch lets us send request from anywhere to any where.
+// which is not good for multiple reasons
+// This means any frontend can send request to any backened
+// This is why these things are blocked
+// if the origins are not same then the request will fail
+
+// to correct that we have route our frontend filr with the same route that we use to send http requests
+
+// now use we require path to provide full the full path basically through this we are just serving our html filr from the same route
+// And make sure we are using it before app.use()
+
+app.get("/", (req,res)=>{
+  res.sendFile(path.join(__dirname,"index.html"))
+})
+
 // for all other routes, return 404
 app.use((req, res, next) => {
   res.status(404).send();
 });
 
+
 app.listen(3000)
+
+
+
+// We can also avoid the cors error by installing the cors library and then
+
+//const cors = require("cors")
+// app.use(cors())
+
+// This enables to send req from any frontend to any backend
