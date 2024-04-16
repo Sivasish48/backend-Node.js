a71ba817ccc5b8064dec143057b6bef331b48859
Sivasish Praharaj <sivasish48@gmail.com>
2024-04-17 01:32:57 +0530

    implement jwt on admin auth

--- 3.0/courseAppOwn/jwtMod.js
+++ 3.0/courseAppOwn/jwtMod.js
@@ -30,6 +30,29 @@ const generateJwt = (user)=>{
 
 }
 
-app.get
+app.post("/admin/signup", (req,res)=>{
+   
+   const admin = req.body
+   console.log(req.body);
+   const existingAdmin = ADMINS.find((a)=>(a.username === admin.username))
+
+   if (existingAdmin){
+      res.json({message:"The admin aleady exists"})
+   } else{
+   ADMINS.push(admin)
+  const token =  generateJwt(admin)    // generates a JSON Web Token (JWT) for the newly created admin.
+  res.json({message:"The admin created successfuly"})
+   }
+})
+
+app.post("/admin/login", (res,req)=>{
+   const { username, password } = req.headers;
+   const admin = ADMINS.find(a => a.username === username && a.password === password);
+ 
+     if(admin){
+      const token = generateJwt(admin)
+      res.json({message:"logged in successfuly"})
+     }
+})
 
 
